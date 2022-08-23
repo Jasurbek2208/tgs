24f28e4cbff2ded1ccf7d94fe9944b007e42b07f
Jasur <jasurbekshomaqsudov2004@gmail.com>
2022-08-23 12:25:31 +0300

    fff

--- src/Components/usermain/UsersMain.tsx
+++ src/Components/usermain/UsersMain.tsx
@@ -6,7 +6,54 @@ import UsersCard from "./usersCard/UsersCard";
 import { UsersStyled } from "./UsersStyled";
 
 export const UsersMain: React.FC = () => {
+  const { Getusers, deletePosition, userPosit, loading } =
+    useContext<IContext>(MyContext);
   const [isopen, setisopen] = useState<boolean>(false);
+  const [checkStore, setCheckStore] = useState<string[]>([]);
+
+  const [curent, setCurent] = useState({
+    _id: "",
+    name: {
+      uz: "",
+      ru: "",
+      en: "",
+    },
+  });
+
+  // DELETE LOGICKASI....
+  function checkedClick(id: string) {
+    if (checkStore.includes(id)) {
+      setCheckStore((p) => p.filter((i) => i !== id));
+    } else {
+      setCheckStore((p) => [...p, id]);
+    }
+  }
+
+  function allChecked(e: React.ChangeEvent<HTMLInputElement>) {
+    if (e.target.checked) {
+      userPosit?.data?.forEach((i: IData) => {
+        if (!checkStore.includes(i._id)) {
+          setCheckStore((p) => [...p, i._id]);
+        }
+      });
+    } else {
+      setCheckStore([]);
+    }
+  }
+
+  function deletePosit() {
+    if (deletePosition) {
+      deletePosition({ ids: checkStore });
+    }
+  }
+  // ========================================
+
+  // get
+  useEffect(() => {
+    if (Getusers) {
+      Getusers();
+    }
+  }, []);
 
   return (
     <UsersStyled>
@@ -16,10 +63,17 @@ export const UsersMain: React.FC = () => {
             <h2>4 Users selected</h2>
           </div>
           <div className="icon--div">
-            <div className="icon icon-icon1"></div>
+            {checkStore.length > 0 ? (
+              <div className="icon icon-icon1" onClick={deletePosit}></div>
+            ) : null}
             <div
-              className="icon icon-icon2"
-              onClick={() => (setisopen(true))}
+              className={
+                "icon " +
+                (checkStore.length === 1 ? "icon-icon2" : "icon-addUser")
+              }
+              onClick={() => {
+                setisopen(true);
+              }}
             ></div>
             <div className="icon icon-icon3"></div>
             <div className="icon icon-icon4"></div>
@@ -35,9 +89,63 @@ export const UsersMain: React.FC = () => {
               <input type="checkbox" />
               <p>Full name</p>
             </div>
+            <div className="expand">
+              <p>Date</p>
+            </div>
+            <div className="expand">
+              <p>Title</p>
+            </div>
+            <div className="expand">
+              <p>Title</p>
+            </div>
+            <div className="expand">
+              <p>Title</p>
+            </div>
+            <div className="expand">
+              <p>Title</p>
+            </div>
+            <div className="expand">
+              <p>Status</p>
+            </div>
           </div>
         </div>
       </section>
+      {loading ? (
+        <Loader />
+      ) : (
+        userPosit?.data?.map((i: any, idx: number) => (
+          <div className="map" key={idx}>
+            <div className="fullName">
+              <input
+                type="checkbox"
+                checked={checkStore.includes(i._id)}
+                onChange={() => {
+                  checkedClick(i._id);
+                  setCurent(i);
+                }}
+              />
+              <p
+                onClick={() => {
+                  setisopen(true);
+                  setCurent(i);
+                }}
+              >
+                {i?.name.uz}
+              </p>
+            </div>
+            <div className="date">
+              <p>{i?.__v}</p>
+            </div>
+            <div className="soha">
+              <p>{i?.name.ru}</p>
+            </div>
+            <div className="brand">
+              <p>{i?.name.en}</p>
+            </div>
+          </div>
+        ))
+      )}
+
       {/* USERS CARD */}
       {/* <UsersCard /> */}
 
